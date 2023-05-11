d39c52eff86885c467af652e4b0407cdf548ef2c
Margret Edeh <sagaedeh@gmail.com>
2023-05-11 11:21:23 -0700

    did the manual search functionality

--- src/component/contact.js
+++ src/component/contact.js
@@ -6,12 +6,13 @@ import { AiOutlineEdit} from 'react-icons/ai';
 function Contact({contacts, setIsedit,isEdit,closemodal, err, setContacts, showAddForm, setShowAddForm}) {
 const [theContact, setTheContact]= useState([])
 const [searchValue, setSearchValue] = useState('');
+const [isfilter, setIsfilter] = useState([])
   useEffect(() => {
     fetch('https://phonebook-backend-production-a67d.up.railway.app/api/v1/phonebook/list-all')
       .then(response => response.json())
       .then(data => setTheContact(data.data))
       .catch(error => console.error(error));
-  }, [theContact]);
+  }, []);
   const handleDeleteContact = (id) => {
     fetch(`https://phonebook-backend-production-a67d.up.railway.app/api/v1/phonebook/delete/${id}`, {
       method: 'DELETE'
@@ -22,23 +23,6 @@ const [searchValue, setSearchValue] = useState('');
         setTheContact(updatedContacts);
         
       })
-      .catch(error => console.error(error));
-  };
-  const handleSearchTermChange = (event) => {
-    const url = 'https://phonebook-backend-production-a67d.up.railway.app/api/v1/phonebook/search';
-    const data = {
-      lastname: event.target.value
-    };
-    const options = {
-      method: 'POST',
-      headers: {
-        'Content-Type': 'application/json'
-      },
-      body: JSON.stringify(data)
-    };
-    fetch(url, options)
-      .then(response => response.json())
-      .then(data => setTheContact(data.data))
       .catch(error => console.error(error));
   };
   const handleEditContact = (id) => {
@@ -98,6 +82,15 @@ const [searchValue, setSearchValue] = useState('');
       })
       .catch(error => console.error(error));
   };
+  const handleSearchTermChange = (event) => {
+  const searchTerm = event.target.value.toLowerCase();
+  setSearchValue(searchTerm);
+
+  const filteredContacts = theContact.filter(contact => contact.lastname.toLowerCase().includes(searchTerm));
+  setIsfilter(filteredContacts)
+};
+
+
 
 
 
@@ -152,7 +145,40 @@ const [searchValue, setSearchValue] = useState('');
 }
     
     <div>
-      {theContact && theContact.map((contact) => {
+      {isfilter === " "? theContact.map((contact) => {
+        const phoneNumberArray = contact.phone_number.split('');
+  
+        // Insert a hyphen after every 3 digits
+        phoneNumberArray.splice(3, 0, '-');
+        phoneNumberArray.splice(7, 0, '-');
+        
+        // Join the array back into a string
+        const formattedPhoneNumber = phoneNumberArray.join('');
+        return (
+          <div key={contact.id} className="bg-white rounded-lg shadow-2xl my-1 p-4">
+      <div className="flex flex-col">
+        <div className="flex items-center justify-between mb-2">
+            <h3 className="text-3xl font-bold">{contact.firstname} {contact.lastname}</h3>
+            <div className='flex gap-3' >
+              <button className="bg-blue-500 rounded-lg w-8 h-8 flex items-center justify-center">
+                <AiOutlineEdit onClick={()=> handleEditContact(contact.id)} className="w-4 h-4 text-white" />
+              </button>
+            <button  onClick={() => handleDeleteContact(contact.id)} 
+            className="bg-red-500 rounded-lg w-8 h-8 flex items-center justify-center ml-auto">
+              <BasketIcon className="w-4 h-4 text-white" />
+            </button>
+            </div>
+        </div>
+        <div className="flex items-center mb-2">
+            <span className="mr-2"><PhoneCallIcon /></span>
+            <h3 className="text-gray-600">{formattedPhoneNumber}</h3>
+        </div>
+      </div>
+          </div>
+        )
+      }):
+      
+      isfilter.map((contact) => {
         const phoneNumberArray = contact.phone_number.split('');
   
         // Insert a hyphen after every 3 digits
